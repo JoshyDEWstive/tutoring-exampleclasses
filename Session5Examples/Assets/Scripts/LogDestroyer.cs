8ed4df0b5f4367c6a43477e4fdd680bd2e7250b8
JoshyDEWstive <joshydewstive@outlook.com>
2020-10-31 10:20:43 +0800

    Session 5

---
+++ Session5Examples/Assets/Scripts/LogDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogDestroyer : MonoBehaviour
+{
+    // We can remove the Start and Update methods because we do not need them
+    public string toDelete = "Log";
+
+    private void OnTriggerEnter(Collider other) {
+        // We need to check if the object is a log, and if it is we need to destroy it.
+
+        string objectName = other.name;
+
+        if(objectName == toDelete ) {
+            Destroy(other.gameObject);
+        }
+
+    }
+
+}
---
+++ Session5Examples/LogSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogSpawner : MonoBehaviour
+{
+
+    public GameObject logPrefab; // Our log prefab
+    public Transform spawnPoint; // Where our log will be spawned
+    public float spawnInterval; // How long between each spawn
+    public bool spawnLogs = true; // Are we currently spawning logs?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("LogSpawn").transform;
+
+        StartCoroutine(LogSpawning());
+    }
+
+    // Note that all this could be optimised to run better, and we can talk about that later
+    IEnumerator LogSpawning() {
+
+        while(true) {
+
+            // Logs should only spawn when spawnLogs is true
+            if(spawnLogs) {
+                // We are creating a new log prefab in the game world, we are setting its position to the spawnpoint
+                // and we are using the default rotation.
+                GameObject newLog = Instantiate(logPrefab);
+                newLog.transform.position = spawnPoint.transform.position;
+                newLog.name = "Log";
+            }
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+    }
+
+}
