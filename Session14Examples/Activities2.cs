dfd8458a4fcdcdbe9980e8cd0f9cfba5e524e351
JoshyDEWstive <joshydewstive@outlook.com>
2020-12-15 18:14:13 +0800

    Session 14 Full

---
+++ Session14Examples/Activities2.cs
@@ -0,0 +1,25 @@
+﻿// Session 14
+// Create a namespace called Session14Activites
+// Declare a class that is inside of that namespace that does not inherit anything
+// Have a public and private integer value
+// Have a method that returns the sum of those values
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Session14Examples/Example2.cs
@@ -0,0 +1,48 @@
+﻿// Implement required namespaces first
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Session14Namespace is the name of the namespace that we want to access
+namespace Session14Namespace
+{
+
+    // Note that this class inherits the monobehaviour class
+    public class Example2 : MonoBehaviour
+    {
+
+        ////////////////////////////////////////////////////////////////////////
+        // Public variables
+
+        public int randomNumberOne = 2;
+        public int randomNumberTwo = 5;
+
+        ////////////////////////////////////////////////////////////////////////
+        // Private variables
+
+        private int _randomNumberThree = 9;
+
+        ////////////////////////////////////////////////////////////////////////
+        // Main methods
+
+        void Start()
+        {
+            //...
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //...
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+        // Helper methods
+
+        private void DoMath(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
