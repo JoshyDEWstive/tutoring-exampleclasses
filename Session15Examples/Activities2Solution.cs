97de5281adbf68cd75790aacab004e2bfab5ba99
JoshyDEWstive <joshydewstive@outlook.com>
2020-12-20 12:05:51 +0800

    Session 15 20/12/2020

---
+++ Session15Examples/Activities2Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities2 : MonoBehaviour
+{
+
+    ////////////////////////////////////////////////////////////////////
+    // Session 15 Activity
+
+    // Change the script that creates a wave so that the colour of the objects are different based on its height
+    // Tip: colour values have to be between 0 and 1
+    // Tip: Sin returns value between -1 and 1
+
+    public GameObject prefabToSpawn;
+    void Start()
+    {
+        SpawnObjectsInWave();
+    }
+
+    // Change this function
+    public void SpawnObjectsInWave() {
+
+        GameObject parentObject = new GameObject("Waves");
+
+        int number = 0;
+        while (number < 50) {
+
+            // Spawn our object
+
+            GameObject objectInLine = Instantiate(prefabToSpawn);
+
+            objectInLine.transform.parent = parentObject.transform;
+
+            // In this case sin will be between 0 and 1, you may want to use this to change your colour!
+            float sinValue = Mathf.Sin(number);
+
+            objectInLine.transform.position = new Vector3(number * 2,2 * sinValue,0);
+
+            MeshRenderer meshRenderer = objectInLine.GetComponent<MeshRenderer>();
+
+            Material oldMaterial = meshRenderer.material;
+            Material newMaterial = new Material(oldMaterial);
+
+            newMaterial.color = new Color(1f * sinValue, 0.3f, 0f);
+
+            meshRenderer.material = newMaterial;
+
+            // Update the condition
+            number += 1;
+
+        }
+    }
+
+
+}
