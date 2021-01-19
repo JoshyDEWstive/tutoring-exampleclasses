0d7074ece769826a698bf0eef9828e2580075604
JoshyDEWstive <joshydewstive@outlook.com>
2021-01-19 18:04:35 +0800

    Session 16 full

---
+++ Session16Examples/Assets/Scripts/Examples/Example2.cs
@@ -0,0 +1,58 @@
+﻿// Copyright things first 
+
+// Implement required namespaces first
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Session14Namespace is the name of the namespace that we want to access
+namespace Session14Namespace
+{
+
+    // Note that this class inherits the monobehaviour class
+    public class Example2 : MonoBehaviour
+    {
+
+        ////////////////////////////////////////////////////////////////////////
+        // Public variables
+
+        public int randomNumberOne = 2;
+        public int randomNumberTwo = 5;
+
+        ////////////////////////////////////////////////////////////////////////
+        // Private variables
+
+        private int _randomNumberThree = 9;
+        // You can also skip writing 'private' and it will by default be a private variable, but it is common practice
+        // to always include the private
+        int _randomNumberFour = 42;
+
+        ////////////////////////////////////////////////////////////////////////
+        // Main methods
+        // Main methods are any method is used frequently by the class and by other classes
+
+        void Start()
+        {
+            //...
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //...
+        }
+
+        ////////////////////////////////////////////////////////////////////////
+        // Helper methods
+        // Helper methods are methods that are used by the main methods to simplify your script to make things quicker
+        // and easier to read. They are usually not accessed by other classes outside the namespace.
+
+        private int DoMath(int a, int b)
+        {
+            int addedNumbers = a + b;
+            return addedNumbers;
+        }
+
+    }
+
+}
---
+++ Session16Examples/Assets/Scripts/JumpCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCheck : MonoBehaviour
+{
+
+    // This script will be on each log and it will check if the character is too close to a log, if it is then the 
+    // level will fail
+
+    private GameController _gameController;
+
+    private void Start() {
+        _gameController = GameController.instance;
+    }
+
+    // Exactly the same as the Log Destroyer script, but instead of the destroying something, we lose
+    // the game
+    private void OnTriggerEnter(Collider other) {
+
+        if(other.CompareTag("Player") && _gameController.gameRunning) _gameController.Lose();
+
+    }
+
+}
