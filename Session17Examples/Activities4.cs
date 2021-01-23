62e2ef62a3f15c7db74c8b327c27840e89746ae5
JoshyDEWstive <joshydewstive@outlook.com>
2021-01-23 17:28:25 +0800

    Session 17 1

---
+++ Session17Examples/Activities4.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities4 : MonoBehaviour
+{
+
+    ////////////////////////////////////////////////////////////
+    // Activity 4
+    // Goal:
+    // Using the distance and the square magnitude function find what object is closer
+
+    // Variables provides
+
+    public Transform objectA;
+    public Transform objectB;
+
+
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DistanceWay()
+    {
+
+    }
+
+    void SqrMagnitudeWay()
+    {
+
+    }
+
+}
