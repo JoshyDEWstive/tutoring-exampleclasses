00ffecfc4aa9f91e08b2c918b1e857794216e7aa
JoshyDEWstive <joshydewstive@outlook.com>
2021-01-25 12:50:57 +0800

    Session17 Full1

---
+++ Session17Examples/Assets/Scripts/Activities/Activities4.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities4 : MonoBehaviour
+{
+
+    ////////////////////////////////////////////////////////////
+    // Activity 4
+    // Goal:
+    // Using the distance and the square magnitude function find what object is closer to the central object
+
+    // Variables provides
+
+    public Transform objectA;
+    public Transform objectB;
+
+    public Transform centralObject;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DistanceWay()
+    {
+        // Find the distance between the central object and objectA
+        float distanceA = Vector3.Distance(centralObject.position, objectA.position);
+
+        // Find the distance between the central object and objectB
+
+        // Compare the distances and output
+
+    }
+
+    void SqrMagnitudeWay()
+    {
+
+    }
+
+}
