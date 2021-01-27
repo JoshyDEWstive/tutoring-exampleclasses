f6c2ab259eef8fcaeaa4a0acb88729dabcffca59
JoshyDEWstive <joshydewstive@outlook.com>
2021-01-27 18:36:31 +0800

    Session 18 Full

---
+++ Session18Examples/Activities3.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities3 : MonoBehaviour
+{
+
+    ////////////////////////////////////////////////////////////
+    // Activity 3
+    // Goal:
+    // Get this script to move towards a game object at a distance
+    // of atleast 5 metres
+
+    // The starting variables have been made for  you
+
+    public float distanceToFollow = 5;
+    public GameObject objectToFollow;
+
+    // Your variables if required
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
---
+++ Session18Examples/Assets/Scripts/Examples/Example6.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example6 : MonoBehaviour
+{
+    ////////////////////////////////////////////////////////////
+    // Distance and moving towards
+
+    // This will return the distance (how far apart) two objects are
+    // You should try not to use this function because it can cause lag but for now it is okay.
+    public float GetDistance(Vector3 objectA,Vector3 objectB)
+    {     
+        return Vector3.Distance(objectA, objectB);
+    }
+
+    // It is better to use this method when comparing distances because you don't need to know the distance
+    // when comparing, just what is bigger than what
+    // This will return the square of the distance
+    public float GetDistanceSquared(Vector3 objectA,Vector3 objectB)
+    {
+        Vector3 offset = objectA - objectB;
+        return Vector3.SqrMagnitude(offset);
+    }
+
+    // This method will will move an object closer to another object at a desired speed
+    public void MoveTowardsObject(Transform objectToMove, Transform moveTo, float speed)
+    {
+        // Make sure to always include the Time.deltaTime multiplication otherwise the method will work too fast for the player
+        // and it will not be smooth
+
+        // The Vector3.MoveTowards method will return a vector that is on a line between the two objects, this vector
+        // will be (speed * Time.deltaTime) closer to the target
+        objectToMove.position = Vector3.MoveTowards(objectToMove.position, moveTo.position, speed * Time.deltaTime);
+    }
+
+}
