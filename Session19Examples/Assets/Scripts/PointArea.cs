90abcc758fbfd0e6fd9e1af4dfc74762880b424c
JoshyDEWstive <joshydewstive@outlook.com>
2021-01-31 20:07:52 +0800

    Session 19 Full

---
+++ Session19Examples/Assets/Scripts/PointArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointArea : MonoBehaviour
+{
+    // This script is a component of the gameobject that has the trigger collider
+
+    private GameController _gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameController = GameController.instance;
+    }
+
+    // When a gameobject enters the trigger collider
+    private void OnTriggerEnter(Collider other) {
+
+        // Checks if the object has the Player tag, if it does, then we we want to let the player get points
+        if (other.CompareTag("Player")) _gameController.canGetPoints = true;
+
+    }
+
+    // When a gameobject leaves the trigger collider
+    private void OnTriggerExit(Collider other) {
+
+        // Checks if the object has the Player tag, if it does, then we we want to stop the player getting points
+        if (other.CompareTag("Player")) _gameController.canGetPoints = false;
+
+    }
+
+}
