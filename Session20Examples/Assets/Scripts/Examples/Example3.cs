b8816b297f09c28c828097bffa2eb3b6012b9a78
JoshyDEWstive <joshydewstive@outlook.com>
2021-02-09 18:13:30 +0800

    Session 20 Full

---
+++ Session20Examples/Assets/Scripts/Examples/Example3.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example3 : MonoBehaviour
+{
+
+    public Material testMaterialOne;
+    public Material testMaterialTwo;
+
+    public GameObject testGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ////////////////////////////////////////////////////////////////////
+        // Changing the material of an object
+
+        // Duplicate the test object for demonstration purposes
+        GameObject newGameObject = Instantiate(testGameObject);
+
+        // Setting the objects name
+        newGameObject.name = "Test Object 2";
+
+        // Getting the mesh renderer component which contains the object's materials
+        MeshRenderer meshRenderer = newGameObject.GetComponent<MeshRenderer>();
+        
+        // The main material of the object, we are changing it to test material two
+        meshRenderer.material = testMaterialTwo;
+
+        // Changing the game object's position
+        newGameObject.transform.position = new Vector3(3, 0, 0);
+
+        ////////////////////////////////////////////////////////////////////
+        // Modifying the values of a material of an object
+
+        // Duplicate the test object for demonstration purposes
+        GameObject newGameObject2 = Instantiate(testGameObject);
+
+        // Setting the objects name
+        newGameObject2.name = "Test Object 3";
+
+        // Getting the mesh renderer component which contains the object's materials
+        MeshRenderer meshRenderer2 = newGameObject2.GetComponent<MeshRenderer>();
+
+        // Creating a new material based on the old material's properties.
+        Material newMaterial = new Material(testMaterialTwo);
+        
+        // Changing the colour of the material
+        newMaterial.color = new Color(0.6f, 0.3f, 0f);
+
+        // To convert the 0-255 value to 0-1 value you can simply divide by 255
+        // So if i had a value of 50, you divide it by 255 and you get 0.194361
+        // new Color(50 / 255,255 / 255 ,0):
+
+        // Changing the material of the renderer to be our new material
+        meshRenderer2.material = newMaterial;
+
+        // Changing the game objects position
+        newGameObject2.transform.position = new Vector3(6, 0, 0);
+
+    }
+
+}
---
+++ Session20Examples/FocusActivity1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusActivity1 : MonoBehaviour
+{
+
+    // Bottomleft corner of the screen
+    public Vector3 bottomLeft;
+    public Vector3 topRight;
+
+    public GameObject treePrefab;
+    public int treesToSpawn = 3;
+
+    private List<GameObject> trees;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        trees = new List<GameObject>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyUp(KeyCode.E)) {
+
+            foreach(GameObject go in trees) {
+                Destroy(go);
+            }
+            treesToSpawn = Random.Range(3, 10);
+
+            trees.Clear();
+            Vector3 myPosition = transform.position;
+
+            float xSize = topRight.x - bottomLeft.x;
+            float ySize = topRight.z - bottomLeft.z;
+
+            for (int i = 0; i < treesToSpawn; i++) {
+
+                GameObject tree = Instantiate(treePrefab);
+
+                float yValue = (bottomLeft.y + topRight.y) / 2;
+
+                float xPos = Random.Range(0, xSize) + bottomLeft.x;
+                float yPos = Random.Range(0, ySize) + bottomLeft.z;
+
+                tree.transform.position = new Vector3(xPos, yValue, yPos);
+
+                trees.Add(tree);
+            }
+
+        }
+
+    }
+
+}
