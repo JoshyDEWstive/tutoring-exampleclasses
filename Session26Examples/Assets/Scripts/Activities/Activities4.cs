06bc75c014d9397dab38a68de8c8d2c5ecc030f0
JoshyDEWstive <joshydewstive@outlook.com>
2021-04-08 18:09:47 +0800

    Session 26 Particle Systems

---
+++ Session26Examples/Assets/Scripts/Activities/Activities4.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activities4 : MonoBehaviour
+{
+
+    ////////////////////////////////////////////////////////////
+    // Activity 4
+    // Goal:
+    // Using the distance and the square magnitude function find what object is closer to the central object
+
+    // Variables provides
+
+    public Transform objectA;
+    public Transform objectB;
+
+    public Transform centralObject;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DistanceWay()
+    {
+
+        // Find the distance between the central object and objectA
+        float distanceA = Vector3.Distance(centralObject.position, objectA.position);
+
+        // Find the distance between the central object and objectB
+        float distanceB = Vector3.Distance(centralObject.position, objectB.position);
+
+        // Compare the distances and output
+        if(distanceA > distanceB) { // A is further than B
+            Debug.Log("B is closer");
+        } else {
+            Debug.Log("A is closer");
+        }
+
+    }
+
+    void SqrMagnitudeWay()
+    {
+        // Calculate the distance between the central object and object A
+
+        Vector3 offsetCA = centralObject.position - objectA.position;
+        float distanceA = Vector3.SqrMagnitude(offsetCA);
+
+    }
+
+}
---
+++ Session26Examples/Assets/Scripts/Activities/Activities5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; // Implement this namespace if you are using interfaces to handle events
+using TMPro; // Don't forget to import this
+
+// We can detect button presses using an interface
+// Add the IPointerClickHandler
+public class Activities5 : MonoBehaviour
+{
+
+    //////////////////////////////////////////////////////
+    // Session 18 Activities 5
+
+    // Goal: Change the text on a string and debug to the console when the button is pressed
+    // Change the text when the button is pressed
+
+    public TextMeshProUGUI textObject;
+
+
+    //  This way is the simplest way!
+    // So the way buttons work is that they will run a certain method or function when an event is ran
+
+    public void ChangeText() {
+        textObject.text = "Anything we want.";
+    }
+
+    // This is the more complex way
+    // Uses IPointerClickHandler
+    public void OnPointerClick(PointerEventData eventData) {
+        ChangeText();
+    }
+
+}
